883843e03898df0e3a81907ebcd08a2e4795c93a
Aakash Kumar <your-email@gmail.com>
2026-01-07 05:23:54 +0530

    leetcode static

--- src/services/leetcodeService.js
+++ src/services/leetcodeService.js
@@ -2,9 +2,25 @@ import fetch from "node-fetch";
 
 const USERNAME = "aakashkumar2005";
 
-export async function fetchLeetCodeStats() {
-  const res = await fetch(
-    `https://leetcode-stats-api.herokuapp.com/${USERNAME}`
-  );
-  return await res.json();
+export async function getLeetCodeStats(req, res) {
+  try {
+    const r = await fetch(
+      `https://leetcode-stats-api.herokuapp.com/${USERNAME}`
+    );
+    const d = await r.json();
+
+    res.json({
+      totalSolved: d.totalSolved,
+      acceptanceRate: d.acceptanceRate,
+      easySolved: d.easySolved,
+      totalEasy: d.totalEasy,
+      mediumSolved: d.mediumSolved,
+      totalMedium: d.totalMedium,
+      hardSolved: d.hardSolved,
+      totalHard: d.totalHard,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "LeetCode fetch failed" });
+  }
 }
